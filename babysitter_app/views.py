63f32106584a95f6e78de96278e3b8dedf69b2e6
SharonChepkoech <sharoncchepkoech@gmail.com>
2025-04-24 19:46:44 +0300

    change import in views - user model

--- babysitter_app/views.py
+++ babysitter_app/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
